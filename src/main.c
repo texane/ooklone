5c8ed19ea5bcf05d5c088c48086ef0b81a642642
texane <texane@gmail.com>
2014-08-05 21:41:05 -0500

    [ update ] continuous ook mode with fixed rssi threshold

--- src/main.c
+++ src/main.c
@@ -6,34 +6,52 @@
 #include "./uart.c"
 
 
+__attribute__((unused)) static uint8_t get_rssi_avg(void)
+{
+  /* note: actual_rssi = - rfm69_get_rssi / 2 */
+  uint16_t i;
+  uint32_t sum = 0;
+  for (i = 0; i != 1000; ++i) sum += rfm69_get_rssi();
+  return (uint8_t)(sum / (uint32_t)i);
+}
+
+
 /* main */
 
 int main(void)
 {
+  uint8_t pre_state = 0xff;
+  uint8_t cur_state;
+  uint16_t i = 0;
+  uint32_t f;
+
   uart_setup();
   rfm69_setup();
+
+  sei();
 
   /* put in rx continuous mode */
   rfm69_set_rx_continuous_mode();
 
-  /* TODO: wait for rxready interrupt, to be wired */
+  uart_write((uint8_t*)"rx\r\n", 4);
 
   while (1)
   {
-    uart_write(uint8_to_string(rfm69_read_reg(0x01)), 2);
-    uart_write((uint8_t*)"\r\n", 2);
+    cur_state = rfm69_get_data();
+    if (cur_state != pre_state)
+    {
+      if ((i & 0xf) == 0)
+      {
+	uart_write((uint8_t*)"\r\n", 2);
+	uart_write(uint16_to_string(i), 4);
+	uart_write((uint8_t*)": ", 2);
+      }
+      ++i;
 
-    uart_write(uint8_to_string(rfm69_read_reg(0x0e)), 2);
-    uart_write((uint8_t*)"\r\n", 2);
-
-    uart_write(uint8_to_string(rfm69_read_reg(0x10)), 2);
-    uart_write((uint8_t*)"\r\n", 2);
-
-    _delay_ms(200);
-    _delay_ms(200);
-    _delay_ms(200);
-    _delay_ms(200);
-    _delay_ms(200);
+      if (cur_state) uart_write((uint8_t*)"1", 1);
+      else uart_write((uint8_t*)"0", 1);
+      pre_state = cur_state;
+    }
   }
 
   return 0;
--- src/rfm69.c
+++ src/rfm69.c
@@ -28,6 +28,11 @@
 #define RFM69_IO_DIO2_PORT PORTD
 #define RFM69_IO_DIO2_MASK (1 << 3)
 
+#define RFM69_IO_DIO4_DDR DDRD
+#define RFM69_IO_DIO4_PIN PIND
+#define RFM69_IO_DIO4_PORT PORTD
+#define RFM69_IO_DIO4_MASK (1 << 4)
+
 static inline void rfm69_csn_setup(void)
 {
   RFM69_IO_CSN_DDR |= RFM69_IO_CSN_MASK;
@@ -102,6 +107,11 @@ static inline void rfm69_write_op_mode(uint8_t x)
   rfm69_write_reg(0x01, x);
 }
 
+static inline uint8_t rfm69_read_op_mode(void)
+{
+  return rfm69_read_reg(0x01);
+}
+
 static inline void rfm69_write_data_modul(uint8_t x)
 {
   rfm69_write_reg(0x02, x);
@@ -122,6 +132,25 @@ static inline void rfm69_write_frf(uint32_t x)
   rfm69_write_reg(0x09, (x >> 0) & 0xff);
 }
 
+static inline void rfm69_read_frf(uint32_t* x)
+{
+  /* carrier frequency */
+
+  *x = ((uint32_t)rfm69_read_reg(0x07)) << 16;
+  *x |= ((uint32_t)rfm69_read_reg(0x08)) << 8;
+  *x |= ((uint32_t)rfm69_read_reg(0x09)) << 0;
+}
+
+static inline void rfm69_write_lna(uint8_t x)
+{
+  rfm69_write_reg(0x18, x);
+}
+
+static inline uint8_t rfm69_read_lna(void)
+{
+  return rfm69_read_reg(0x18);
+}
+
 static inline void rfm69_write_ook_peak(uint8_t x)
 {
   rfm69_write_reg(0x1b, x);
@@ -137,8 +166,47 @@ static inline void rfm69_write_ook_fix(uint8_t x)
   rfm69_write_reg(0x1d, x);
 }
 
+static inline uint8_t rfm69_read_rssi_config(void)
+{
+  return rfm69_read_reg(0x23);
+}
+
+static inline void rfm69_write_rssi_config(uint8_t x)
+{
+  rfm69_write_reg(0x23, x);
+}
+
+static inline uint8_t rfm69_read_rssi_value(void)
+{
+  return rfm69_read_reg(0x24);
+}
+
+static inline uint8_t rfm69_get_rssi(void)
+{
+  rfm69_write_rssi_config(1 << 0);
+  while ((rfm69_read_rssi_config() & (1 << 1)) == 0) ;
+  return rfm69_read_rssi_value();
+}
+
+static inline void rfm69_write_dio_mapping_2(uint8_t x)
+{
+  rfm69_write_reg(0x26, x);  
+}
+
+static inline uint8_t rfm69_read_dio_mapping_2(void)
+{
+  return rfm69_read_reg(0x26);
+}
+
+static inline uint8_t rfm69_read_irq_flags_1(void)
+{
+  return rfm69_read_reg(0x27);
+}
+
 static void rfm69_setup(void)
 {
+  uint8_t x;
+
   spi_setup_master();
   rfm69_csn_setup();
   rfm69_csn_high();
@@ -146,31 +214,51 @@ static void rfm69_setup(void)
   RFM69_IO_DIO2_DDR &= ~RFM69_IO_DIO2_MASK;
   RFM69_IO_DIO2_PORT &= ~RFM69_IO_DIO2_MASK;
 
+  RFM69_IO_DIO4_DDR &= ~RFM69_IO_DIO4_MASK;
+  RFM69_IO_DIO4_PORT &= ~RFM69_IO_DIO4_MASK;
+
   /* put in standby mode */
   rfm69_write_op_mode(1 << 2);
 
   /* bitrate */
   /* bitrate = fxosc / bitrate */
   /* fxosc = 32MHz */
-#define RFM69_BITRATE_KBPS 10
-  rfm69_write_bitrate((uint16_t)(32000.0 / (double)RFM69_BITRATE_KBPS));
+#define RFM69_BITRATE_KBPS 10.0
+  rfm69_write_bitrate((uint16_t)(32000.0 / RFM69_BITRATE_KBPS));
 
   /* 433.92 MHz carrier frequency */
   /* fcar = fstep * frf */
   /* fstep = fxosc / (2^19) */
   /* fxosc = 32MHz */
-  rfm69_write_frf((uint32_t)(433.92 / 32.0));
+  rfm69_write_frf((uint32_t)7109345.28);
 
- /* TODO: rfm69_write_ook_peak(); */
+  /* fixed ook threshold */
+  /* TODO: should be in a calibration loop */
+  rfm69_write_ook_peak(0);
  /* TODO: rfm69_write_ook_avg(); */
- /* TODO: rfm69_write_ook_fix(); */
+  rfm69_write_ook_fix(0xf0 / 4);
+
+  /* set dio4 mapping as for rx ready */
+  x = rfm69_read_dio_mapping_2();
+  x = (x & ~(3 << 6)) | (1 << 6);
+  rfm69_write_dio_mapping_2(x);
+
+  x = rfm69_read_lna();
+  x = (x & ~7) | 2;
+  rfm69_write_lna(x);
 }
 
 static void rfm69_set_rx_continuous_mode(void)
 {
   rfm69_write_data_modul((3 << 5) | (1 << 3));
-  rfm69_write_op_mode(4 << 2);
+  rfm69_write_op_mode((1 << 7) | (4 << 2));
+  while (!(rfm69_read_irq_flags_1() & (1 << 7))) ;
 }
 
+static inline uint8_t rfm69_get_data(void)
+{
+  return RFM69_IO_DIO2_PIN & RFM69_IO_DIO2_MASK;
+}
+ 
 
 #endif /* RFM69_C_INCLUDED */
